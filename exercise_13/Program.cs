718073f18a63cff6daade64acd0915f87b8f634c
Ratimir <ra0953029364@gmail.com>
2019-12-08 10:17:45 +0200

    Добавьте файлы проекта.

---
+++ exercise_13/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("введите 5 чисел ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            int n5 = int.Parse(Console.ReadLine());
+            int average = (n1 + n2 + n3 + n4 + n5)/5;
+            Console.WriteLine($"Среднее арифметическое {average} ");
+            Console.ReadLine();
+            /*Console.Write("введите n чисел ");
+            int n = int.Parse(Console.ReadLine());
+            int[] average = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"введите {i} элемент массива ");
+                average[i] = int.Parse(Console.ReadLine());
+                int result  = average[i] + average[i] / i;
+                Console.WriteLine(average);
+            }*/
+
+
+        }
+    }
+}
